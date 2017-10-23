3e2e69aedbf22bbd0dce831c8c65bac65f442b5f
Daniele Demichelis <d.demichelis@techgap.it>
2017-10-23 11:06:45 +0200

    Added directive example

--- angualrjs-1.6-helloworld/app/app.js
+++ angualrjs-1.6-helloworld/app/app.js
@@ -1,7 +1,48 @@
 var myApp = angular
+
     .module('myApp', [])
         .controller('FirstController', function FirstController($scope){
+
             $scope.messages = [
                 'Hello World!', 'This is a message from Angular'
             ];
-        });
+
+        })
+
+        .controller('PhoneController', function PhoneController($scope){
+
+            $scope.phones = {
+                nokia: {
+                    model: "7110",
+                    ram: 2
+                },
+                xiaomi: {
+                    model: "mi2",
+                    ram: 3
+                }
+            };
+
+        })
+
+    .directive('myDirective', function () {
+        return {
+            template: "<strong>mydirective</strong>"
+        }
+    })
+
+    .directive('myPhone', function () {
+        return {
+            restrict: 'E',
+            template: '<table border="1"><tr><td colspan="2">Phone details</td></tr><tr><td>{{phone.model}}</td><td>{{phone.ram}}</td></tr></table>',
+            scope: {
+                // 'key' attribute has to be used in the tag.
+                // It sould resolve to an object from controller's $scope to be
+                // associated to the 'phone' object that is private to the directive.
+                //
+                phone: '=key'
+            }
+        }
+    })
+
+
+;
