23b6a0d1a1a2fd0acf21321f51a7644d80557c5f
Daniele Demichelis <d.demichelis@techgap.it>
2017-10-24 10:50:53 +0200

    Redux works!

--- redux/basic_example/app/app.js
+++ redux/basic_example/app/app.js
@@ -1,47 +1,3 @@
-function myReducer(currentState, action){
-
-
-    // if(currentState == undefined){
-    //     currentState = {
-    //         value: 0,
-    //         multiplier: 1
-    //     }
-    // }
-
-    //if(action.type.startsWith("@@redux") || currentState == undefined) return;
-
-    switch(action.type) {
-        case 'INCREMENT':
-            return {
-                value: currentState.value + currentState.multiplier,
-                multiplier: currentState.multiplier
-            }
-        case 'DECREMENT':
-            return {
-                value: currentState.value - currentState.multiplier,
-                multiplier: currentState.multiplier
-            }
-        case 'INCREMENT_MULTIPLIER':
-            return {
-                value: currentState.value,
-                multiplier: currentState.multiplier * 10
-            }
-        case 'DECREMENT_MULTIPLIER':
-            return {
-                value: currentState.value,
-                multiplier: currentState.multiplier * 0.1
-            }
-        //default:
-          //  throw "Unknown action '" + action.type + "'";
-    }
-}
-
-var store = Redux.createStore(myReducer, {
-        value: 0,
-        multiplier: 1
-});
-console.log("initial state:" + store.getState());
-
 function getElementById(id) {
     var elementById = window.document.getElementById(id);
     if(elementById==null) {
@@ -50,18 +6,56 @@ function getElementById(id) {
     return elementById;
 }
 
-var incBtn;
-var decBtn;
-var incMultBtn;
-var decMultBtn;
-var valueDiv;
-
 window.onload = function () {
-    incBtn = getElementById('increment');
-    decBtn = getElementById('decrement');
-    incMultBtn = getElementById('incrementMultiplier');
-    decMultBtn = getElementById('decrementMultiplier');
-    valueDiv = getElementById('value');
+
+    /**
+     * When the action is unknow, a state that is equivalent to the
+     * given one should be returned.
+     */
+    function myReducer(currentState, action){
+
+        switch(action.type) {
+            case 'INCREMENT':
+                return {
+                    value: currentState.value + currentState.multiplier,
+                    multiplier: currentState.multiplier
+                };
+            case 'DECREMENT':
+                return {
+                    value: currentState.value - currentState.multiplier,
+                    multiplier: currentState.multiplier
+                };
+            case 'INCREMENT_MULTIPLIER':
+                return {
+                    value: currentState.value,
+                    multiplier: currentState.multiplier * 10
+                };
+            case 'DECREMENT_MULTIPLIER':
+                return {
+                    value: currentState.value,
+                    multiplier: currentState.multiplier * 0.1
+                };
+            case 'FORCE_UI_UPDATE':
+                return currentState;
+            default:
+                return currentState;
+
+        }
+    }
+
+    var initialState = {
+        value: 0,
+        multiplier: 1
+    };
+    var store = Redux.createStore(myReducer, initialState);
+    console.log("# Initial state:");
+    console.log(store.getState());
+
+    var incBtn = getElementById('increment');
+    var decBtn = getElementById('decrement');
+    var incMultBtn = getElementById('incrementMultiplier');
+    var decMultBtn = getElementById('decrementMultiplier');
+    var valueDiv = getElementById('value');
 
     incBtn.onclick = function() {
         store.dispatch( {type: 'INCREMENT'} );
@@ -86,6 +80,8 @@ window.onload = function () {
 
         valueDiv.textContent = newState.value;
     } );
+
+    store.dispatch( {type: 'FORCE_UI_UPDATE'} );
 }
 
 
